8e9b671ed065904b78c1a8c560e9200459e7cfd7
jeanpitx <yojean02@hotmail.com>
2021-09-21 11:00:43 -0500

    correccion errores

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,9 +1,9 @@
 <tr>
 <td class="header">
 @if (trim($slot) === 'Laravel')
-<img src="https://cdn-icons-png.flaticon.com/512/418/418154.png" alt="{{config('app.name', 'Laravel')}}" style="width:45%">
+<img src="https://cdn-icons-png.flaticon.com/512/418/418154.png" alt="{{config('app.name', 'Laravel')}}" style="width:90px">
 @else
-<img src="https://cdn-icons-png.flaticon.com/512/418/418154.png" alt="{{config('app.name', 'Laravel')}}" style="width:45%">
+<img src="https://cdn-icons-png.flaticon.com/512/418/418154.png" alt="{{config('app.name', 'Laravel')}}" style="width:90px">
 <!--{{ $slot }}-->
 @endif
 </td>
